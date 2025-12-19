cfbb83720c41cdd84887d2768a42d6ed0f8127e8
Chinmay Kumar Panda <126417086+ChinmayKumarPanda@users.noreply.github.com>
2025-12-19 15:39:13 +0530

    Update LT.tf

--- LT.tf
+++ LT.tf
@@ -1,7 +1,4 @@
-resource "aws_iam_role_policy_attachment" "cloudwatch_agent" {
-  role       = aws_iam_role.ec2_s3_role.name
-  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
-}
+
 resource "aws_iam_role" "ec2_s3_role" {
   name = "wp-ec2-s3-role"
 
